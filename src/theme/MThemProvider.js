a83089358553ee7cf70bbc95135ccb3818147016
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-28 17:39:06 +0700

    Update MThemProvider.js

--- src/theme/MThemProvider.js
+++ src/theme/MThemProvider.js
@@ -1,42 +1,33 @@
-
 import { CssBaseline } from "@mui/material";
-import {
-    createTheme,
-    ThemeProvider,
-} from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const PRIMARY = {
     main: "#d74742",
-    contrastText: "#fff"
+    contrastText: "#fff",
 };
 const SECONDARY = {
     main: "#292FF2",
-    contrastText: "#fff"
+    contrastText: "#fff",
 };
 const SUCCESS = {
     main: "#1DF267",
-    contrastText: "#000"
-}
+    contrastText: "#000",
+};
 const ACTIVE = {
     active: "#d74742",
-    contrastText: "#000"
-}
+    contrastText: "#000",
+};
 const ERROR = {
     main: "#F23C35",
-    contrastText: "#000"
-}
+    contrastText: "#000",
+};
 const WARNING = {
     main: "#F2D34E",
-    contrastText: "#000"
-}
-const VARIANTBTN = {
-
-}
-
+    contrastText: "#000",
+};
+const VARIANTBTN = {};
 
 function MThemeProvider({ children }) {
-
-
     const themeOptions = createTheme({
         palette: {
             primary: PRIMARY,
@@ -44,59 +35,67 @@ function MThemeProvider({ children }) {
             action: ACTIVE,
             success: SUCCESS,
             error: ERROR,
-            warning: WARNING
+            warning: WARNING,
         },
-    })
-    const theme = createTheme({
-        components: {
-            MuiButton: {
-                styleOverrides: {
-                    root: {
-                        marginBottom: "1rem",
+    });
+    const theme = createTheme(
+        {
+            components: {
+                MuiButton: {
+                    styleOverrides: {
+                        root: {
+                            marginBottom: "1rem",
+                        },
                     },
+                    variants: [
+                        {
+                            props: { variant: "main" },
+                            style: {
+                                "&:hover": {
+                                    backgroundColor:
+                                        themeOptions.palette.primary.main,
+                                    color: "#fff",
+                                },
+                                color: "#000",
+                            },
+                        },
+                        {
+                            props: { variant: "success" },
+                            style: {
+                                "&:hover": {
+                                    backgroundColor:
+                                        themeOptions.palette.success.light,
+                                },
+                                "&:focus": {
+                                    outline: "none",
+                                },
+                                color: themeOptions.palette.success
+                                    .contrastText,
+                                backgroundColor:
+                                    themeOptions.palette.success.main,
+                            },
+                        },
+                        {
+                            props: { variant: "error" },
+                            style: {
+                                "&:hover": {
+                                    backgroundColor:
+                                        themeOptions.palette.error.light,
+                                },
+                                "&:focus": {
+                                    outline: "none",
+                                },
+                                color: themeOptions.palette.error.contrastText,
+                                backgroundColor:
+                                    themeOptions.palette.error.main,
+                            },
+                        },
+                    ],
                 },
-                variants: [
-                    {
-                        props: { variant: "main" },
-                        style: {
-                            "&:hover": {
-                                backgroundColor: themeOptions.palette.primary.main,
-                                color: "#fff"
-                            },
-                            color: "#000"
-                        }
-                    },
-                    {
-                        props: { variant: "success" },
-                        style: {
-                            "&:hover": {
-                                backgroundColor: themeOptions.palette.success.light,
-                            },
-                            "&:focus": {
-                                outline: "none",
-                            },
-                            color: themeOptions.palette.success.contrastText,
-                            backgroundColor: themeOptions.palette.success.main,
-                        }
-                    },
-                    {
-                        props: { variant: "error" },
-                        style: {
-                            "&:hover": {
-                                backgroundColor: themeOptions.palette.error.light,
-                            },
-                            "&:focus": {
-                                outline: "none",
-                            },
-                            color: themeOptions.palette.error.contrastText,
-                            backgroundColor: themeOptions.palette.error.main,
-                        }
-                    }
-                ]
-            }
-        }
-    }, themeOptions)
-
+            },
+        },
+        themeOptions
+    );
 
     return (
         <ThemeProvider theme={theme}>
