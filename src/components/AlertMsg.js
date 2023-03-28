87408d8a668b1e46c53de6e70b1366c7ed5078f7
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-28 17:38:43 +0700

    Update AlertMsg.js

--- src/components/AlertMsg.js
+++ src/components/AlertMsg.js
@@ -3,14 +3,14 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const AlertMsg = () => {
-  return (
-    <ToastContainer
-      position="top-right"
-      hideProgressBar={false}
-      newestOnTop={true}
-      pauseOnHover
-    />
-  );
+    return (
+        <ToastContainer
+            position="top-right"
+            hideProgressBar={false}
+            newestOnTop={true}
+            pauseOnHover
+        />
+    );
 };
 
 export default AlertMsg;
