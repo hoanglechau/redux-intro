4e9f94866428db583a5ac97e81dc8bfdc3621d3e
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-28 17:38:47 +0700

    Update WrapperBox.js

--- src/components/WrapperBox.js
+++ src/components/WrapperBox.js
@@ -4,7 +4,7 @@ import { styled } from "@mui/material/styles";
 const WrapperBox = styled(Box)(({ theme }) => ({
     border: `1px solid ${theme.palette.primary.main}`,
     boxShadow: "2px 2px 7px 1px #95a5a6",
-    textAlign: "center"
-}))
+    textAlign: "center",
+}));
 
-export default WrapperBox
+export default WrapperBox;
