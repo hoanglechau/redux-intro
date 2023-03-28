46b5d239c0922eb50e188e0ad73a1353dd630ac2
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-28 17:38:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,24 +9,28 @@ import ReduxFinal from "./pages/ReduxFinal";
 import PublicLayout from "./layout/PublicLayout";
 import MThemeProvider from "./theme/MThemProvider";
 
-
-
 function App() {
-  return (
-    <BrowserRouter>
-      <MThemeProvider>
-        <Routes>
-          <Route path="/" element={<PublicLayout />}>
-            <Route index element={<PropDrillingExercise />} />
-            <Route path="prop-final" element={<PropDrillingFinal />} />
-            <Route path="redux-exercise" element={<ReduxExercise />} />
-            <Route path="redux-final" element={<ReduxFinal />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </MThemeProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <MThemeProvider>
+                <Routes>
+                    <Route path="/" element={<PublicLayout />}>
+                        <Route index element={<PropDrillingExercise />} />
+                        <Route
+                            path="prop-final"
+                            element={<PropDrillingFinal />}
+                        />
+                        <Route
+                            path="redux-exercise"
+                            element={<ReduxExercise />}
+                        />
+                        <Route path="redux-final" element={<ReduxFinal />} />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Route>
+                </Routes>
+            </MThemeProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
