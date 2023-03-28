4e2b8330ce591539502d4b8629c2faab02ef0943
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-28 17:38:49 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 // import store from "./redux/store";
-import store from "./service/store"
+import store from "./service/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
 );
